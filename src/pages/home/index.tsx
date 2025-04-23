db203c0f274bfc8585342008f48a35c997e73a67
chenxu <chenxu4012@foxmail.com>
2025-04-23 16:44:09 +0800

    feat(home): 添加课程数据动态加载和样式优化
    在主页中添加了从Appwrite数据库动态加载课程数据的功能，并优化了部分样式，包括课程卡片宽度和文本对齐方式。这些改动提升了页面的动态性和用户体验。

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react';
 import './index.scss';
+import { databases } from '../../utils/appwrite';
+import { Course } from '../../../types/db';
+import { CollectionName, DatabaseName } from '../../../types/enums';
+import { Query } from 'appwrite';
+import { useNavigate } from 'react-router';
 
 function Home() {
+    const [course, setCourse] = useState<Course[]>([])
+    const navigate = useNavigate()
+    useEffect(() => {
+        const getData = async () => {
+            const { documents } = await databases.listDocuments<Course>(
+                DatabaseName.ai_stem,
+                CollectionName.course,
+                [
+                    Query.orderDesc('$createdAt'),
+                    Query.limit(3),
+                    Query.offset(0)
+                ]
+            );
+            setCourse(documents);
+        }
+        getData();
+    }, [])
     return (
         <div className="istem-home-box">
             <div className="box_3 flex-row">
@@ -35,20 +58,16 @@ function Home() {
                         </div>
                         <div className="card-content flex-row justify-between">
                             <div className="bullet-points flex-col justify-between">
-                                {[1, 2, 3].map((item) => (
-                                    <div key={item} className="bullet-point" />
+                                {course.map((item) => (
+                                    <div className='flex-row'>
+                                        <div key={item.$id} className="bullet-point" ></div>
+                                        <span className='bullet-text'>{item.name}</span>
+                                    </div>
                                 ))}
-                            </div>
-                            <div className="content-text">
-                                Impact of an adaptime dialog to default students' ideas and guide knowl-
-                                <br />
-                                New Feature Uint Tags
-                                <br />
-                                Technology Update April 17.2024
                             </div>
                         </div>
                         <div className="card-footer">
-                            <span className="view-more">查看更多</span>
+                            <span onClick={() => navigate('/course-preview/course-list')} className="view-more">查看更多</span>
                         </div>
                     </div>
                 </div>
@@ -72,9 +91,9 @@ function Home() {
                 <div className="box_5 flex-row">
                     <div className="box_6 flex-col" />
                     <div className="block_10 flex-col justify-between">
-                <span className="text_14">
-                  本研究旨在开发一款&nbsp;i-STEM&nbsp;智能辅导系统,该系统致力于通过&nbsp;A1与个性化的学习体验来提升学生的问题解决能力。我们结合了设计思维方法与人工智能技术，为小学阶段的学生提供了一个独特且高效的&nbsp;STEM&nbsp;学习平台。
-                </span>
+                        <span className="text_14">
+                            本研究旨在开发一款&nbsp;i-STEM&nbsp;智能辅导系统,该系统致力于通过&nbsp;A1与个性化的学习体验来提升学生的问题解决能力。我们结合了设计思维方法与人工智能技术，为小学阶段的学生提供了一个独特且高效的&nbsp;STEM&nbsp;学习平台。
+                        </span>
                         <div className="section_7 flex-row align-center justify-center">
                             <div className="image-text_8 flex-row justify-between">
                                 <span className="text-group_2">了解更多</span>
@@ -110,9 +129,9 @@ function Home() {
                             <div className="text-group_7-0 flex-col justify-between">
                                 <span className="text_15-0">个性化学习资源</span>
                                 <span className="text_16-0">
-                    利用&nbsp;AI&nbsp;技术，系统能够根据每位学生的不同学习偏好和风格，提供多样化的学习材料，包括文本、图片和视频等
-                    <br />
-                  </span>
+                                    利用&nbsp;AI&nbsp;技术，系统能够根据每位学生的不同学习偏好和风格，提供多样化的学习材料，包括文本、图片和视频等
+                                    <br />
+                                </span>
                             </div>
                         </div>
                         <div className="image-text_4-1 flex-col">
@@ -120,9 +139,9 @@ function Home() {
                             <div className="text-group_7-1 flex-col justify-between">
                                 <span className="text_15-1">动态适应性内容</span>
                                 <span className="text_16-1">
-                    导师能够根据学生的学习表现、参与度和支持需求，动态调整学习内容，并推荐最适合的学习资料。
-                    <br />
-                  </span>
+                                    导师能够根据学生的学习表现、参与度和支持需求，动态调整学习内容，并推荐最适合的学习资料。
+                                    <br />
+                                </span>
                             </div>
                         </div>
                         <div className="image-text_4-2 flex-col">
@@ -130,9 +149,9 @@ function Home() {
                             <div className="text-group_7-2 flex-col justify-between">
                                 <span className="text_15-2">设计思维融入</span>
                                 <span className="text_16-2">
-                    本项目的一大创新点在于将设计思维的五个核心步骤-共情、定义创意、原型制作和测试-融入到系统的开发过程中，以确保系统不仅高效而且具有高度的用户友好性。
-                    <br />
-                  </span>
+                                    本项目的一大创新点在于将设计思维的五个核心步骤-共情、定义创意、原型制作和测试-融入到系统的开发过程中，以确保系统不仅高效而且具有高度的用户友好性。
+                                    <br />
+                                </span>
                             </div>
                         </div>
                         <div className="image-text_4-3 flex-col">
@@ -140,8 +159,8 @@ function Home() {
                             <div className="text-group_7-3 flex-col justify-between">
                                 <span className="text_15-3">互动式问题解决练习</span>
                                 <span className="text_16-3">
-                    学生将获得量身定制的问题解决练习，以及对其进情况的注细反馈，这有助干提高他们的参与度和技能发展。
-                  </span>
+                                    学生将获得量身定制的问题解决练习，以及对其进情况的注细反馈，这有助干提高他们的参与度和技能发展。
+                                </span>
                             </div>
                         </div>
                     </div>
@@ -171,10 +190,10 @@ function Home() {
                         />
                     </div>
                     <span className="paragraph_2">
-                本系统基于最新的教育技术和心理学理论构建，特别关注于改善小学生的科学知识、方法及高级认知技能。通过将A驱动的个性化学习与设计思维框架结合，我们力求创造一个既新颖又有效的&nbsp;STEM&nbsp;学习环境，激发孩子们对科学、技术、工程和数学的兴趣，培养他们成为未来社会的创新者和领导者。
-                <br />
-                感谢您的访问，如果您有任何疑问或建议，欢迎随时联系我们!
-              </span>
+                        本系统基于最新的教育技术和心理学理论构建，特别关注于改善小学生的科学知识、方法及高级认知技能。通过将A驱动的个性化学习与设计思维框架结合，我们力求创造一个既新颖又有效的&nbsp;STEM&nbsp;学习环境，激发孩子们对科学、技术、工程和数学的兴趣，培养他们成为未来社会的创新者和领导者。
+                        <br />
+                        感谢您的访问，如果您有任何疑问或建议，欢迎随时联系我们!
+                    </span>
                 </div>
             </div>
 
